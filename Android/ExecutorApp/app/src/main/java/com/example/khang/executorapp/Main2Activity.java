db3618c33a35339ddc9710c8161b16eedfd32166
khangit96 <khangit69@gmail.com>
2018-03-08 19:16:21 +0700

    fjfsj

--- Android/ExecutorApp/app/src/main/java/com/example/khang/executorapp/Main2Activity.java
+++ Android/ExecutorApp/app/src/main/java/com/example/khang/executorapp/Main2Activity.java
@@ -4,14 +4,18 @@ import android.app.Dialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,75 +26,90 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.geniusforapp.fancydialog.FancyAlertDialog;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import static com.example.khang.executorapp.NotificationHelper.NEGATIVE_CLICK;
 import static com.example.khang.executorapp.NotificationHelper.POSITIVE_CLICK;
 
-public class Main2Activity extends AppCompatActivity {
-RelativeLayout rl;
-String content="";
+public class Main2Activity extends AppCompatActivity implements OnMapReadyCallback {
+    List<DataSnapshot> dtList;
+    private GoogleMap mMap;
+    List<Marker> originMarkers = new ArrayList<>();
+    List<Marker> destinationMarkers = new ArrayList<>();
+    List<Polyline> polylinePaths = new ArrayList<>();
+    LatLng duyTanUni;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Bundle bd = getIntent().getBundleExtra("BD_COQUAN");
-        CoQuan coQuan = (CoQuan) bd.getSerializable("COQUAN");
+        final CoQuan coQuan = (CoQuan) bd.getSerializable("COQUAN");
         setTitle(coQuan.ten);
-        rl= (RelativeLayout) findViewById(R.id.rl);
+        dtList = new ArrayList<>();
 
         final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
-        mDatabase.child("LoaiPhanAnhChinh/" + bd.getInt("POS") + "/coQuan/" + coQuan.key + "/Emergency").addChildEventListener(new ChildEventListener() {
+        mDatabase.child("LoaiPhanAnhChinh/" + bd.getInt("POS") + "/coQuan/" + coQuan.key + "/Emergency").addValueEventListener(new ValueEventListener() {
             @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                if(s!=null){
-                    Log.d("test","demo");
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                dtList.clear();
+                for (DataSnapshot dt : dataSnapshot.getChildren()) {
+                    dtList.add(dt);
+                    Log.d("test", dt.getValue().toString());
                 }
+                //1 description
+                //0 location emergency
+                //6 keyEmergency
+                //7 keyEvent
+                //9 latitude emergency
+                //10 longtitude emergency
+                //8 longtitude agency
+                //17 latitude agency
+                if (dtList.size() > 0) {
 
-                Snackbar snackbar = Snackbar
-                        .make(rl, "Emergency Notification", Snackbar.LENGTH_LONG)
-                        .setDuration(Snackbar.LENGTH_INDEFINITE)
-                        .setAction("Accept", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                Snackbar snackbar1 = Snackbar.make(rl, "Message is restored!", Snackbar.LENGTH_SHORT);
-                                snackbar1.show();
-                            }
-                        });
-
-
-                // Changing message text color
-                snackbar.setActionTextColor(Color.RED);
-
-                // Changing action button text color
-                View sbView = snackbar.getView();
-                TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
-                textView.setTextColor(Color.YELLOW);
-
-                snackbar.show();
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-                Toast.makeText(getApplicationContext(), "Changed", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
+                    double latitudeAgency = Double.parseDouble((dtList.get(17).getValue().toString()));
+                    double longtitudeAgency = Double.parseDouble((dtList.get(8).getValue().toString()));
+                    LatLng latLngAgency = new LatLng(latitudeAgency, longtitudeAgency);
+                    String currentLocation = "" + duyTanUni.latitude + "," + duyTanUni.longitude;
+                    String des = "" + latitudeAgency + "," + longtitudeAgency;
+                    getDataFromGoogleMapServer(currentLocation, des);
+                    mMap.addMarker(new MarkerOptions().position(duyTanUni).title("Emergency Location"));
+                    mMap.addMarker(new MarkerOptions().position(latLngAgency).title(coQuan.ten).icon(BitmapDescriptorFactory.fromResource(R.drawable.congan)));
+                    showHeadsUpNotification(dtList.get(1).getValue().toString(), dtList.get(7).getValue().toString(), dtList.get(6).getValue().toString());
+                }
             }
 
             @Override
@@ -100,51 +119,27 @@ String content="";
         });
     }
 
-    public void sendNoti() {
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this);
-        Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, intent, 0);
-        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        mBuilder.setContentIntent(pendingIntent);
-
-        mBuilder.setSmallIcon(R.mipmap.binhduongems);
-        mBuilder.setSubText("Emergency Notification");
-        mBuilder.setContentTitle("Duy Tan University");
-        Intent acceptPendingIntent = new Intent();
-        acceptPendingIntent.setAction("ACCEPT");
-        PendingIntent pIntentAccept = PendingIntent.getBroadcast(this, 1234, acceptPendingIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        PendingIntent dismissIntent = PendingIntent.getBroadcast(getApplicationContext(), 3, acceptPendingIntent, PendingIntent.FLAG_CANCEL_CURRENT);
-        mBuilder.addAction(R.drawable.binhduongems, "Accept", pIntentAccept);
-        mBuilder.addAction(R.drawable.binhduongems, "Refuse", dismissIntent);
-        mBuilder.setSound(alarmSound);
-        mBuilder.setAutoCancel(true);
-        mBuilder.setContentText("dkdkdk");
-
-        NotificationManager mNotificationManager =
-
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        mNotificationManager.notify(3, mBuilder.build());
-    }
-
-    public void showHeadsUpNotification(String content) {
+    public void showHeadsUpNotification(String content, String keyEvent, String keyEmergency) {
         int notificationId = new Random().nextInt();
 
         Intent intent = new Intent(this, Main2Activity.class);
         PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
-        Intent positive = new Intent(this, NotificationReceiver.class);
-        positive.putExtra("notiID", notificationId);
-        positive.setAction(POSITIVE_CLICK);
+        Intent acceptIntent = new Intent(this, NotificationReceiver.class);
+        acceptIntent.putExtra("notiID", notificationId);
+        acceptIntent.putExtra("KEY_EVENT", keyEvent);
+        acceptIntent.putExtra("KEY_EMERGENCY", keyEmergency);
+        acceptIntent.setAction(POSITIVE_CLICK);
 
-        PendingIntent pIntent_positive = PendingIntent.getBroadcast(this, notificationId, positive, PendingIntent.FLAG_CANCEL_CURRENT);
+        PendingIntent pIntent_positive = PendingIntent.getBroadcast(this, notificationId, acceptIntent, PendingIntent.FLAG_CANCEL_CURRENT);
 
-        Intent negative = new Intent(this, NotificationReceiver.class);
-        negative.putExtra("notiID", notificationId);
-        negative.setAction(NEGATIVE_CLICK);
+        Intent refuseIntent = new Intent(this, NotificationReceiver.class);
+        refuseIntent.putExtra("notiID", notificationId);
+        refuseIntent.putExtra("KEY_EVENT", keyEvent);
+        refuseIntent.putExtra("KEY_EMERGENCY", keyEmergency);
+        refuseIntent.setAction(NEGATIVE_CLICK);
 
-        PendingIntent pIntent_negative = PendingIntent.getBroadcast(this, notificationId, negative, PendingIntent.FLAG_CANCEL_CURRENT);
+        PendingIntent pIntent_negative = PendingIntent.getBroadcast(this, notificationId, refuseIntent, PendingIntent.FLAG_CANCEL_CURRENT);
 
 
         NotificationCompat.Builder notification = NotificationHelper.createNotificationBuider(this,
@@ -153,9 +148,173 @@ String content="";
         notification.setPriority(Notification.PRIORITY_HIGH).setVibrate(new long[0]);
         Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         notification.setSound(alarmSound);
-        notification.addAction(new NotificationCompat.Action(R.drawable.ic_notifications, "Postive", pIntent_positive));
-        notification.addAction(new NotificationCompat.Action(R.drawable.ic_notifications, "Negative", pIntent_negative));
+        notification.addAction(new NotificationCompat.Action(R.drawable.ic_notifications, "Accept", pIntent_positive));
+        notification.addAction(new NotificationCompat.Action(R.drawable.ic_notifications, "Refuse", pIntent_negative));
 
         NotificationHelper.showNotification(this, notificationId, notification.build());
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        duyTanUni = new LatLng(16.060761, 108.208175);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(duyTanUni, 15f));
+
+
+        // getDataFromGoogleMapServer();
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                return false;
+            }
+        });
+    }
+
+    /*
+ * Parse JSon get direction from google map server
+ * */
+    public boolean getDataFromGoogleMapServer(String currentLocation, String des) {
+        final Location destinationLocation = new Location("destinationLocation");
+        destinationLocation.setLatitude(MyService.viDo);
+        destinationLocation.setLongitude(MyService.kinhDo);
+
+        final ProgressDialog progressDialog = new ProgressDialog(Main2Activity.this);
+        progressDialog.setMessage("Getting path...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+//        String currentLocation = "11.053428,106.668529";
+//        String des = "11.0517096,106.6679601";
+        String SERVER_GOOGLEMAP = "https://maps.googleapis.com/maps/api/directions/json?origin=" + currentLocation + "&destination=" + des + "&key=AIzaSyD5Xzis5_DOGW2XLYvOQ7FCVvFzLsym9aA";
+
+        final RequestQueue requestQueue = Volley.newRequestQueue(Main2Activity.this);
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, SERVER_GOOGLEMAP, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject jsonObject) {
+                        try {
+                            JSONArray jsonRoutes = jsonObject.getJSONArray("routes");
+                            JSONObject jsonRoute = jsonRoutes.getJSONObject(0);
+                            JSONObject overview_polylineJson = jsonRoute.getJSONObject("overview_polyline");
+                            JSONArray jsonLegs = jsonRoute.getJSONArray("legs");
+                            JSONObject jsonLeg = jsonLegs.getJSONObject(0);
+                            //     JSONObject jsonDistance = jsonLeg.getJSONObject("distance");
+                            String points = overview_polylineJson.getString("points");
+                            drawPolyline(destinationLocation, decodePolyLine(points));
+                            progressDialog.dismiss();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.dismiss();
+                    }
+                });
+        requestQueue.add(jsObjRequest);
+        return true;
+    }
+
+    /*
+    *
+    * */
+
+    public void drawPolyline(Location location, List<LatLng> points) {
+        if (originMarkers != null) {
+            for (Marker marker : originMarkers) {
+                marker.remove();
+            }
+        }
+
+        if (destinationMarkers != null) {
+            for (Marker marker : destinationMarkers) {
+                marker.remove();
+            }
+        }
+
+        if (polylinePaths != null) {
+            for (Polyline polyline : polylinePaths) {
+                polyline.remove();
+            }
+        }
+
+        polylinePaths = new ArrayList<>();
+        destinationMarkers = new ArrayList<>();
+
+        LatLng endLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        Marker marker = mMap.addMarker(new MarkerOptions()
+                .position(endLocation)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.congan)));
+        destinationMarkers.add(marker);
+        PolylineOptions polylineOptions = new PolylineOptions()
+                .geodesic(true)
+                .color(getResources().getColor(R.color.colorAccent))
+                .width(15);
+
+        for (int i = 0; i < points.size(); i++)
+            polylineOptions.add(points.get(i));
+        polylinePaths.add(mMap.addPolyline(polylineOptions));
+//        getCurrentLocation(15f);
+//        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+//            @Override
+//            public boolean onMarkerClick(Marker marker) {
+//                if (marker.getTitle() != null) {
+//                    showBottonSheet(Integer.parseInt(marker.getTitle()));
+//                }
+//                previousMaker = marker;
+//                marker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.ic_house_selected));
+//
+//                return true;
+//            }
+    }
+
+
+    /*
+    *
+    * */
+    private List<LatLng> decodePolyLine(final String poly) {
+        int len = poly.length();
+        int index = 0;
+        List<LatLng> decoded = new ArrayList<LatLng>();
+        int lat = 0;
+        int lng = 0;
+
+        while (index < len) {
+            int b;
+            int shift = 0;
+            int result = 0;
+            do {
+                b = poly.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = poly.charAt(index++
+
+                ) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            decoded.add(new LatLng(
+                    lat / 100000d, lng / 100000d
+            ));
+        }
+
+        return decoded;
     }
 }
--- Android/ExecutorApp/app/src/main/java/com/example/khang/executorapp/NotificationReceiver.java
+++ Android/ExecutorApp/app/src/main/java/com/example/khang/executorapp/NotificationReceiver.java
@@ -9,7 +9,12 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
+import android.util.Log;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by khang on 07/03/2018.
@@ -18,23 +23,31 @@ public class NotificationReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
-        int noti_id=intent.getIntExtra("notiID",0);
+        int noti_id = intent.getIntExtra("notiID", 0);
+        String keyEvent = intent.getStringExtra("KEY_EVENT");
+        String keyEmergency = intent.getStringExtra("KEY_EMERGENCY");
+        String status = "";
+        if (action.equals(NotificationHelper.POSITIVE_CLICK)) {
 
-        if(action.equals(NotificationHelper.POSITIVE_CLICK)) {
-            Toast.makeText(context,"Positive click",Toast.LENGTH_LONG).show();
-           // Log.d("Action Positive","Pressed");
-            cancel(context,noti_id);
-        } else if(action.equals(NotificationHelper.NEGATIVE_CLICK)) {
-            Toast.makeText(context,"Negative click",Toast.LENGTH_LONG).show();
-            cancel(context,noti_id);
+            //   Toast.makeText(context,"Positive click",Toast.LENGTH_LONG).show();
+            Log.d("test", keyEvent + ":" + keyEmergency);
+            status = "Processing";
+            cancel(context, noti_id);
+
+        } else if (action.equals(NotificationHelper.NEGATIVE_CLICK)) {
+            //   Toast.makeText(context,"Negative click",Toast.LENGTH_LONG).show();
+            Log.d("test", keyEvent + ":" + keyEmergency);
+            status = "Refuse";
+            cancel(context, noti_id);
+        } else if (action.equals(NotificationHelper.REPLY_CLICK)) {
+            // Log.d("Action Reply","Pressed"+getMessageText(intent));
+            cancel(context, noti_id);
         }
-        else if(action.equals(NotificationHelper.REPLY_CLICK)) {
-           // Log.d("Action Reply","Pressed"+getMessageText(intent));
-            cancel(context,noti_id);
-        }
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("SuKienPhanAnh/" + keyEvent + "/Emergency/" + keyEmergency).child("status").setValue(status);
     }
 
-    public static void cancel(Context context, int id){
+    public static void cancel(Context context, int id) {
         NotificationManager notificacaoManager = (NotificationManager) context.getSystemService(Activity.NOTIFICATION_SERVICE);
         notificacaoManager.cancel(id);
     }
